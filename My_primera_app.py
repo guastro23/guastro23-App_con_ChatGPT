4af62a0d5905f0856606c9d9a96710e5f4d74d20
jean_Carlos <112786732+guastro23@users.noreply.github.com>
2023-08-28 16:45:06 -0500

    Update My_primera_app.py

--- My_primera_app.py
+++ My_primera_app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+
+def main():
+    st.title("Mi primera app")
+    st.write("Autor: Esta app fue elaborada por COLOQUE AQUÍ SU NOMBRE")
+
+    user_name = st.text_input("Ingresa tu nombre", "Escribe aquí...")
+
+    if user_name:
+        st.write(f"{user_name}, te doy la bienvenida a mi primera app.")
+
+if __name__ == "__main__":
+    main()
 
